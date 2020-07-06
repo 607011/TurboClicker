9e570f52f8f2a007a05a4bfda2a1be8466b0b86c
Oliver Lau <oliver@ersatzworld.net>
2020-07-06 08:06:54 +0200

    Initial Commit

---
+++ Tap/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tap
+//
+//  Created by Oliver Lau on 06.07.20.
+//  Copyright © 2020 Oliver Lau. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tap
+//
+//  Created by Oliver Lau on 06.07.20.
+//  Copyright © 2020 Oliver Lau. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
